d6ef534bdc61aa5e291dfb8e74c526607f0cda73
Hyeoninii <hyeoninii0314@gmail.com>
2025-07-05 21:12:41 +0900

    feat:change dashboard UI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import App from './App.jsx'
+import UploadImage from './UploadImage.jsx'
 import Dashboard from './Dashboard.jsx'
 import './index.css'
 
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/upload" element={<App />} />
+        <Route path="/upload" element={<UploadImage />} />
         <Route path="/" element={<Navigate to="/dashboard" />} />
         <Route path="*" element={<Navigate to="/dashboard" />} />
       </Routes>
